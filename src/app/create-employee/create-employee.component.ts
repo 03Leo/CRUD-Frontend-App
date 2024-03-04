c1f5209934e8b70338c21b344e030e33dec466f7
Hritik Jaiswal <hk4hritikjaiswal@gmail.com>
2024-03-04 13:14:26 +0530

    Added Employee List

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,12 +11,28 @@ import { Employee } from '../employee';
 export class CreateEmployeeComponent implements OnInit {
 
   employee: Employee = new Employee();
-  constructor() {}
+  constructor(private employeeeService: EmployeeService,
+    private router: Router) {}
 
   ngOnInit(): void {
   }
 
+  saveEmployee(){
+    this.employeeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data);
+      this.gotoEmployeeList();
+    },
+    error => console.log(error));
+  }
+
+  gotoEmployeeList(){
+    this.router.navigate(['/employees']);
+
+  }
+
   onSubmit(){
+    console.log(this.employee);
+    this.saveEmployee();
 
   }
 
